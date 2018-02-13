94482d6e70f7592c194011e69b1787a1fa71784f
Sarthak More <ssmore12@gmail.com>
2018-02-12 20:21:56 -0800

    Add files via upload

--- src/js/Globe.js
+++ src/js/Globe.js
@@ -9,17 +9,33 @@ function setArc3D(pointStart, pointEnd, smoothness, color, clockWise) {
   normal.copy(cb).normalize();
 
 
+
   var angle = pointStart.angleTo(pointEnd); // get the angle between vectors
   if (clockWise) angle = angle - Math.PI * 2;  // if clockWise is true, then we'll go the longest path
   var angleDelta = angle / (smoothness - 1); // increment
 
+  var myRand = getRndInteger(3, 6);
+  var myRand2 = getRndInteger(3, 6);
+
+
   var geometry = new THREE.Geometry();
   for (var i = 0; i < smoothness; i++) {
-    geometry.vertices.push(pointStart.clone().applyAxisAngle(normal, angleDelta * i))  // this is the key operation
+    var mynorm = new THREE.Vector3();
+    mynorm.set(Math.sin((myRand * Math.PI) * i / 2999) / (Math.PI / myRand2), normal.y, normal.z);
+    mynorm.normalize();
+    geometry.vertices.push(pointStart.clone().applyAxisAngle(mynorm, angleDelta * i))  // this is the key operation
   }
 
   var arc = new THREE.Line(geometry, new THREE.LineBasicMaterial({
     color: color
   }));
   return arc;
+}
+
+function getRndInteger(min, max) {
+  var n = Math.floor(Math.random() * (max - min + 1) ) + min;
+  if (Math.random() >= 0.5) {
+    n = n * -1;
+  }
+  return n
 }
