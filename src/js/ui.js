18b49b7240ba33c954c4cac34dbe793d6629259d
Bryan Wong <bryanw0ng@ucla.edu>
2018-02-14 15:16:40 -0800

    playing with title screen interaction

--- src/js/ui.js
+++ src/js/ui.js
@@ -11,6 +11,51 @@ const UIController = (function(){
 	quoteAnswer = document.getElementById('quoteAnswer'),
 	quoteClose = quoteBoxMain.getElementsByClassName('close')[0];
 
+	/* TITLE SCREEN */
+
+	var titleBlur = 0;
+	var opacity = 1;
+	var mousedownID = -1;  //Global ID of mouse down interval
+	function mousedown(event) {
+
+		if(mousedownID==-1)  //Prevent multiple loops!
+			mousedownID = setInterval(whilemousedown.bind(this), 25 /*execute every 100ms*/);
+
+	}
+
+	function mouseup(event) {
+	   if(mousedownID!=-1) {  //Only stop if exists
+	     clearInterval(mousedownID);
+	     mousedownID=-1;
+	   }
+
+	   var interval = setInterval(function(){
+	   		if(titleBlur <= 0){
+	   			clearInterval(interval);
+	   			return;
+	   		}
+		   	titleBlur--;
+			var el = this;
+			el.style.filter = "blur(" + titleBlur + "px)";
+	   }.bind(this), 25);
+
+	}
+
+	function whilemousedown() {
+	   titleBlur++;
+	   var el = this;
+	   el.style.filter = "blur(" + titleBlur + "px)";
+
+	   opacity -= .005;
+	   el.style.opacity = opacity;
+	}
+
+	title.addEventListener("mousedown", mousedown);
+	title.addEventListener("mouseup", mouseup);
+	title.addEventListener("mouseout", mouseup);
+
+	/* USER INPUT ANSWER SCREEN */
+
 	function onAnswerSubmit(e){
 
 		if(e)
@@ -22,23 +67,6 @@ const UIController = (function(){
 		title.classList.add('fadeOut');
 
 		return false;
-	}
-
-	function showQuoteMain(data){
-
-		let otherAns = quoteBoxMain.getElementsByClassName('quoteAnswer')[0],
-		username = quoteBoxMain.getElementsByClassName('username')[0];
-
-		otherAns.innerHTML = data.quote;
-		username.innerHTML = "-" + data.username;
-
-		quoteBoxMain.classList.add('fadeIn');
-
-	}
-
-	function hideQuoteMain(){
-		quoteBoxMain.classList.remove('fadeIn');
-		quoteBoxMain.classList.add('fadeOut');
 	}
 
 	function ansKeyDown(e){
@@ -62,9 +90,29 @@ const UIController = (function(){
 	}
 
 	userSubmitQuote.addEventListener('mousedown', onAnswerSubmit);
-	quoteClose.addEventListener('mousedown', hideQuoteMain);
 	answer.addEventListener('keydown', ansKeyDown);
 	answer.addEventListener('blur', keepBlur);
+
+	/* QUOTE PAGE FROM OTHER USERS */
+
+	function showQuoteMain(data){
+
+		let otherAns = quoteBoxMain.getElementsByClassName('quoteAnswer')[0],
+		username = quoteBoxMain.getElementsByClassName('username')[0];
+
+		otherAns.innerHTML = data.quote;
+		username.innerHTML = "-" + data.username;
+
+		quoteBoxMain.classList.add('fadeIn');
+
+	}
+
+	function hideQuoteMain(){
+		quoteBoxMain.classList.remove('fadeIn');
+		quoteBoxMain.classList.add('fadeOut');
+	}
+
+	quoteClose.addEventListener('mousedown', hideQuoteMain);
 
 	return{
 		showQuoteMain: showQuoteMain,
