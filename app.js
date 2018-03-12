0001a55c7fbcd15bf6578086e10ec5e79fbdfdd7
Michael Yu <michael.yu2016@gmail.com>
2018-03-11 23:28:05 -0700

    finished payments

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 const path = require('path');
 const API = require('./api');
 
+
+/* cross origin */
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Auth-Token, Content-Type, Accept");
+  next();
+});
+
 app.set('view engine', 'ejs');
 
 app.use('/', express.static(path.join(__dirname + '/public')));
